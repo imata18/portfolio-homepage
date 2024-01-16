6c2932b7b70aad14bbdc98b59127ab7bf30ad548
ivan_mata <imata@bu.edu>
2024-01-16 16:36:12 -0500

    Setup basic page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import './App.css';
-import { Container, Grid } from '@mui/material';
+import React from "react";
+import "./App.css";
+import { Container, Grid } from "@mui/material";
 
-import Profile from './Components/Profile/Profile';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import Portfolio from './Pages/Portfolio/Portfolio';
-import Resume from './Pages/Resume/Resume';
+import Profile from "./Components/Profile/Profile";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Container> 
-      <Grid container> 
-        <Grid item lg={3} md={3} sm={12} xs={12}> 
+    <Container>
+      <Grid container>
+        <Grid item lg={3} md={3} sm={12} xs={12}>
           <Profile />
         </Grid>
         <Grid item lg={9} md={9} sm={12} xs={12}>
           <Header />
-          <Portfolio />
-          <Resume />
+          <Router>
+            <Routes>
+              <Route path="/portfolio" element={<Portfolio />}></Route>
+              <Route path="/" element={<Resume />}></Route>
+            </Routes>
+          </Router>
           <Footer />
         </Grid>
       </Grid>
