70d963b3a8dcf1d3accf98a66fb0384d7115c648
ivan_mata <imata@bu.edu>
2024-01-16 15:22:05 -0500

    Setupt page container and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Container, Grid } from '@mui/material';
+
+import Profile from './Components/Profile/Profile';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Resume from './Pages/Resume/Resume';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container> 
+      <Grid container> 
+        <Grid item lg={3} md={3} sm={12} xs={12}> 
+          <Profile />
+        </Grid>
+        <Grid item lg={9} md={9} sm={12} xs={12}>
+          <Header />
+          <Portfolio />
+          <Resume />
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
