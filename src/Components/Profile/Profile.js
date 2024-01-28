4e71383b364b5989235e60fb7af252bb21256c64
ivan_mata <imata@bu.edu>
2024-01-28 16:12:26 -0500

    Added in variables for name on title

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,14 +2,17 @@ import { Typography } from "@mui/material";
 import React from 'react'
 import CustomTimeline from '../Timeline/Timeline'
 
+import jobInfo from "../../Utils/jobInfo";
+
 import './Profile.css'
+
 
 const Profile = () => {
   return (
     <div className="profile containter_shadow">
       <div className="profile_name">
-        <Typography className="name">Temp</Typography>
-        <Typography className="title">Temp</Typography>
+        <Typography className="name">{jobInfo.name}</Typography>
+        <Typography className="title">{jobInfo.title}</Typography>
       </div>
       <figure className="profile_image">
         <img src={require('../../Assets/Images/Portfolio_pic.jpg')} alt="" />
---
+++ src/Utils/jobInfo.js
@@ -0,0 +1,4 @@
+export default {
+    name: 'Ivan Mata',
+    title: 'Recent MS Graduate',
+}
