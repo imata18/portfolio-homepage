9ef42585f1f0038c3892d45caee09f9560cd0d6b
ivan_mata <imata@bu.edu>
2024-01-28 18:19:31 -0500

    Added in jobInfo variables

--- src/Utils/jobInfo.js
+++ src/Utils/jobInfo.js
@@ -1,4 +1,20 @@
 export default {
     name: 'Ivan Mata',
     title: 'Recent MS Graduate',
+
+    birthday: 'August 18th, 1999',
+    email: 'imata@bu.edu',
+    phone: '(713) 870-2036',
+    address: 'Boston, MA, USA',
+
+    socials: {
+        github: {
+            link: 'https://github.com/imata18',
+            text: 'imata18'
+        },
+        linkedin: {
+            link: 'https://www.linkedin.com/in/ivan-mata-574608174/',
+            text: 'Ivan Mata'
+        }
+    }
 }
