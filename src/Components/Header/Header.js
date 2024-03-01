43517a88e63a05b17b73b805c6bd59bab0c51c80
ivan_mata <imata@bu.edu>
2024-02-29 20:17:21 -0500

    First Attempt at Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 const Header = () => {
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div>
-        Header
-    </div>
+    <Tabs 
+    value={value}
+        onChange={handleChange}
+      className='header'
+      //value={value}
+      //onChange={handleChange}
+      TabIndicatorProps={{
+        style: {
+          backgroundColor: "white"
+        }
+      }}
+      TabTextProps={{
+        style: {
+          color: "white"
+        }
+      }}
+      //aria-label="secondary tabs example"
+    >
+      <Tab value="one" label="Item One" />
+      <Tab value="two" label="Item Two" />
+    </Tabs>
   )
 }
 
