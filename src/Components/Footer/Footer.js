d5b9d3ec31174182290edbd9dbc78107a1330431
ivan_mata <imata@bu.edu>
2024-04-27 15:06:58 -0400

    Footer linkedin link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Footer.css'
+import { Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className = "footer">
+        <div className = 'footer_left'>
+          <Typography className='footer_name'></Typography>   
+        </div>
+        <div className = 'footer_right'>
+            <Typography className='footer_copyright'>
+                Designed and Devleoped by <a href='https://www.linkedin.com/in/ivan-mata-574608174/' target='_blank'>Ivan Mata</a>
+                <br/>
+                Original design by <a href='https://themeforest.net/' target='_blank'>ThemeForest</a>
+            </Typography>   
+        </div>
     </div>
   )
 }
