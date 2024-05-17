d58fd157e0ed74b995b774fb27e001f91925f54e
ivan_mata <imata@bu.edu>
2024-05-17 19:19:22 -0400

    Send Message section added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,45 +1,66 @@
-import React from 'react'
+import React from "react";
 
-import { Grid } from '@mui/material';
-import { Typography } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import './Contact.css';
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import "./Contact.css";
+import CustomButton from "../../Components/Button/Button";
 
 const Contact = () => {
   return (
     <>
-        <Grid container className='section'>
+      <Grid container className="section">
+        <Grid item xs={12} lg={7}>
+          <Grid container>
             {/* Contact Form */}
-            <Grid item className='section_title mb_30'>
-            <span></span>
-            <h6 className='section_title_text'>Contact Form</h6>
+            <Grid item className="section_title mb_30">
+              <span></span>
+              <h6 className="section_title_text">Contact Form</h6>
             </Grid>
             <Grid item xs={12}>
-                <Grid container className='contact_form'>
-                    <Grid item xs={12} lg={3}> 
-                        <TextField className='input_field' label="Name" variant="standard" />
-                    </Grid>
-                    <Grid item xs={12} lg={3}> 
-                        <TextField className='input_field' label="Email" variant="standard" />
-                    </Grid>
+              <Grid container spacing={3} className="contact_form">
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    className="input_field"
+                    label="Name"
+                    variant="standard"
+                  />
                 </Grid>
-                <TextField
-                    className='input_field top_30'
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    fullWidth
+                    className="input_field"
+                    label="Email"
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    className="input_field"
                     label="Message"
-                    multiline 
+                    multiline
                     rows={4}
-                />
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <CustomButton className="btn_sendMessage" text='Send Message'/>
+                </Grid>
+              </Grid>
             </Grid>
-
-            {/* Contact Information */}
-            <Grid item xs={12} lg={5}> 
-            </Grid>
+          </Grid>
         </Grid>
 
-        {/* Address - Google Map */}
-        
-    </>
-  )
-}
+        {/* Contact Information */}
+        <Grid item xs={12} lg={5}>
 
-export default Contact
+        </Grid>
+      </Grid>
+
+      {/* Address - Google Map */}
+    </>
+  );
+};
+
+export default Contact;
