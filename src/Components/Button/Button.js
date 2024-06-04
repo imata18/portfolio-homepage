83d9c0a557215e20f42035e8a2ca14f92a09c5a3
ivan_mata <imata@bu.edu>
2024-06-04 19:18:12 -0400

    Fixed email validation and button submit functionality

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,12 +2,13 @@ import React from "react";
 import Button from  '@mui/material/Button';
 
 import "./Button.css";
-const CustomButton = ({text, icon, onClick}) => {
+const CustomButton = ({text, icon, onClick, type}) => {
   return (
     <Button
       className="custom_btn"
       endIcon={icon ? <div className="btn_icon_ctner">{icon}</div> : null}
-      onClick={onClick}>
+      onClick={onClick}
+      type={type}>
       <span className="btn_text">{text}</span>
     </Button>
   );
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-import { Grid } from "@mui/material";
-import { Typography } from "@mui/material";
+import { Grid , Typography , Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import "./Contact.css";
 import CustomButton from "../../Components/Button/Button";
 import contactInfo from "../../Utils/contactInfo";
 import jobInfo from "../../Utils/jobInfo.js";
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(
+    "Thank you for reaching out. I'll get back to you shortly."
+  );
+};
 
 const Contact = () => {
   return (
@@ -19,42 +25,45 @@ const Contact = () => {
               <span></span>
               <h6 className="section_title_text">Contact Form</h6>
             </Grid>
-            <Grid item xs={12}>
-              <Grid container spacing={3} className="contact_form">
-                <Grid item xs={12} md={6}>
-                  <TextField
-                    fullWidth
-                    required
-                    className="input_field"
-                    label="Name"
-                    variant="standard"
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextField
-                    fullWidth
-                    type="email"
-                    required
-                    className="input_field"
-                    label="Email"
-                    variant="standard"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    required
-                    className="input_field"
-                    label="Message"
-                    multiline
-                    rows={4}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <CustomButton className="btn_sendMessage" text='Send Message'/>
+            <form onSubmit={handleSubmit}>
+              <Grid item xs={12}>             
+                <Grid container spacing={3} className="contact_form">             
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      fullWidth
+                      required
+                      className="input_field"
+                      label="Name"
+                      variant="standard"
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <TextField
+                      fullWidth
+                      type="email"
+                      required
+                      className="input_field"
+                      label="Email"
+                      variant="standard"
+                      name="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      required
+                      className="input_field"
+                      label="Message"
+                      multiline
+                      rows={4}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <CustomButton type="submit" className="btn_sendMessage" text='Send Message'/>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
+            </form>
           </Grid>
         </Grid>
 
