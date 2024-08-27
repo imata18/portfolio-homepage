3c3432efbab88753beb580452ddbd617ac2fae16
ivan_mata <imata@bu.edu>
2024-08-27 15:33:18 -0400

    Spacing fix

--- src/Utils/educationInfo.js
+++ src/Utils/educationInfo.js
@@ -4,14 +4,14 @@ export default {
             school: 'Boston University',
             degree: 'Master of Science in Computer Science',
             date: '2021 - 2023',
-            relevantCourses: 'Relevant Courses: User-Centric Systems, Object-oriented Software Principles and Design,' +
+            relevantCourses: 'Relevant Courses: User-Centric Systems, Object-Oriented Software Principles and Design, ' +
             'Mobile Application Development, Intro to Natural Language Processing, Image and Video Computing, Databases'
         }, {
             school: 'Boston University',
             degree: 'Bachelor of Science in Linguistics and Computer Science',
             date: '2017 - 2021',
-            relevantCourses: 'Relevant Courses: Probability in Computer Science, Multivariable Calculus, Differential Equations,' +
-            'Artificial Intelligence, Linear Algebra, Computer Systems, Software Engineering, Computational Linguistics,' +
+            relevantCourses: 'Relevant Courses: Probability in Computer Science, Multivariable Calculus, Differential Equations, ' +
+            'Artificial Intelligence, Linear Algebra, Computer Systems, Software Engineering, Computational Linguistics, ' +
             'Computer Graphics, Methods of Scientific Computing, Methods of Applied Mathematics'
         }
     ]
